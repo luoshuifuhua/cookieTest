350b25c76c6f20a06f2390963fda587d31582dd7
HiulHao <hh_prime@outlook.com>
2022-08-04 16:42:57 +0800

    new class: Mybits.Class

---
+++ src/main/java/Mybits.java
@@ -0,0 +1,11 @@
+/**
+ * @author H_H
+ * @date 2022/08/04 16:40
+ **/
+public class Mybits {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
